0521a2cf4125f2c2edcec1566f23c1c8e11c9465
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-05-29 10:23:42 +0530

    imageUpload routes created

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+const { cloudinary } = require("./routes/admin/utils/cloudinary");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
 
 //importing Routes
 const authRoutes = require("./routes/auth/route");
 
 const port = process.env.PORT || 9000;
-const app = express();
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(process.env.MONGO_URL, {
