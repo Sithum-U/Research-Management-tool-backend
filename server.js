9f43254fce8b14515f7d321cdfc157c5ffa0ce5e
Buddhini-123 <buddhiniup123@gmail.com>
2022-05-31 16:27:09 +0530

    panel created

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const panelMemberRoutes = require("./routes/panel_member/panelMemberRoute");
+const panelRoutes = require("./routes/panel_member/panelRoute");
 const presentationRoutes = require("./routes/panel_member/presentationRoute");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {useUnifiedTopology:true,
@@ -17,6 +20,7 @@ mongoose.connect(process.env.MONGO_URL, {useUnifiedTopology:true,
 
 app.use('/panelMember',panelMemberRoutes);
 app.use('/presentation',presentationRoutes);
+app.use('/panel',panelRoutes);
 
 app.listen(port, function() {
     console.log(`Server started on port ${port}`);
