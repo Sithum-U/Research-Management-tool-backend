0018e801ad815ba74d7b911f8a9274eaa3bcd56d
Sithum-U <avsithum@gmail.com>
2022-05-31 15:10:27 +0530

    Supervisor model created

---
+++ models/supervisors/supervisorModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const schema = mongoose.Schema;
+
+const supervisorSchema = new schema({
+    supName: {
+        type: String,
+        required: true
+    },
+    supEmail: {
+        type: String,
+        required: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) throw new Error("Email is invalid...!");
+        }
+    },
+    supPassword: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    supResearchField: {
+        type: String,
+        required: true,
+    },
+    supPhone: {
+        type: String,
+        required: true,
+        validate(value) {
+          if (!validator.isMobilePhone(value, "si-LK", true))
+            throw new Error("Phone number is invalid!!!");
+        }
+    }
+});
+
+const Supervisor = mongoose.model("supervisor", supervisorSchema);
+module.exports = Supervisor;
