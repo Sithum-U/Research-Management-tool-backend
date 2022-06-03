aea11870f2fd2a71b067a288611a7e1e1b15e12d
Piyumi0 <it20158804@my.sliit.lk>
2022-06-03 12:11:27 +0530

    backend created

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const CreateGroup = require("./routes/student/createGroupRout");
+const Findtopic = require("./routes/student/findTopicRoute");
+const Docsubmit = require("./routes/student/docSubmitRoute");
+
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL, {useUnifiedTopology:true}).then(() => {
     console.log("Database connected successfully")
 });
+app.use("/creategroup",CreateGroup) 
+app.use("/findtopic",Findtopic)
+app.use("/docsubmit",Docsubmit)
 
 app.listen(port, function() {
     console.log(`Server started on port ${port}`);
